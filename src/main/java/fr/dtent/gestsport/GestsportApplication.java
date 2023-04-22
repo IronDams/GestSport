5920cf8e4a11a5174c801050d155cef1f153ce3b
IronDams <dams87150@gmail.com>
2023-04-22 14:36:36 +0200

    [Initialisation] - First commit

---
+++ src/main/java/fr/dtent/gestsport/GestsportApplication.java
@@ -0,0 +1,13 @@
+package fr.dtent.gestsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestsportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dtent/gestsport/GestsportApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dtent.gestsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
