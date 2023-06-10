acac9aeff9b9fc34a9afb29a38a922af2309ecd0
IronDams <dams87150@gmail.com>
2023-06-11 00:02:14 +0200

    Modification de la gestion de la securité

--- src/main/java/fr/dtent/gestsport/configuration/SpringSecurityConfig.java
+++ src/main/java/fr/dtent/gestsport/configuration/SpringSecurityConfig.java
@@ -2,36 +2,44 @@ package fr.dtent.gestsport.configuration;
 
 import javax.management.relation.Role;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfiguration {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("springuser").password(passwordEncoder().encode("spring123")).roles("ROLE_USER")
-            .and()
-            .withUser("springadmin").password(passwordEncoder().encode("admin123")).roles("ROLE_ADMIN", "ROLE_USER");
+public class SpringSecurityConfig {
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder.encode("password"))
+            .roles("ADMIN")
+            .build();
+
+
+        return new InMemoryUserDetailsManager(admin);
     }
     
-    private PasswordEncoder passwordEncoder() {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-            .antMatchers("/admin").hasRole("ROLE_ADMIN")
-            .antMatchers("/users").hasRole("ROLE_USER")
             .anyRequest().authenticated()
             .and()
-            .formLogin();
+            .httpBasic();
+        return http.build();
     }
 }
